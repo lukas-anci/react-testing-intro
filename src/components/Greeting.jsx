a6cbc03d48495c3221fd5ed96e94108575ff56ea
Lukas <lukas.ancikevicius@gmail.com>
2021-09-06 08:57:21 +0300

    getAllBy

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -2,6 +2,8 @@ export default function Greeting() {
   return (
     <div>
       <h2>Hello world</h2>
+      {/* <h2>Hello world</h2> */}
+      <p>We are learning Testing React</p>
     </div>
   );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,11 +1,21 @@
 import Greeting from './Greeting';
 import { render, screen } from '@testing-library/react';
-// Unit test
-it('Should contain Hello world', () => {
-  // Arrange
-  render(<Greeting />);
-  // Act
-  // Assert
-  const titleEl = screen.getByText('Hello world');
-  expect(titleEl).toBeInTheDocument();
+
+describe('<Greeting/>', () => {
+  // Unit test
+  it('Should contain Hello world', () => {
+    // Arrange
+    render(<Greeting />);
+    // Act
+    // Assert
+    const titleEl = screen.getByText('Hello world');
+    expect(titleEl).toBeInTheDocument();
+  });
+
+  //   it('Should be 2 titles', () => {
+  //     render(<Greeting />);
+
+  //     const twoTitleArr = screen.getAllByText('Hello world');
+  //     expect(twoTitleArr.length).toBe(2);
+  //   });
 });
